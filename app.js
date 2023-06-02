a05d3ea92e6d047c4f912f3a4f86b1ea5443ea6d
sahanenes <sahanenesss@gmail.com>
2023-06-02 21:16:51 +0300

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function add(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
-console.log(add(1, 6));
-console.log(add("1", "6"));
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", function () {
+    var num1 = num1Element === null || num1Element === void 0 ? void 0 : num1Element.value;
+    var num2 = num2Element === null || num2Element === void 0 ? void 0 : num2Element.value;
+    var result = add(+num1, +num2);
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,7 +1,16 @@
-function add(num1, num2) {
+const num1Element = document.getElementById("num1") as HTMLInputElement;
+const num2Element = document.getElementById("num2") as HTMLInputElement;
+
+const buttonElement = document.querySelector("button");
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
+buttonElement?.addEventListener("click", () => {
+  const num1 = num1Element?.value;
+  const num2 = num2Element?.value;
 
-console.log(add('1', '6'));
+  const result = add(+num1, +num2);
+  console.log(result);
+});
